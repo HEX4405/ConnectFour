b1da1f618b8ca9cd770f53c131d5564141609ebd
thomasfernandez <fernandez.thomas.f@gmail.com>
2015-10-13 16:20:07 +0200

    Implements vertical and horizontal winning conditions.

--- connectFour.pl
+++ connectFour.pl
@@ -4,6 +4,8 @@ run :-
 	write('==============================='), nl, nl,
 	play([[],[],[],[],[],[],[]], x).
 
+play(Board, Player) :- otherPlayer(Player, Opponent), gameOver(Board, Opponent), nl, displayWinner(Board, Opponent), !.
+
 play(Board, Player) :-
 	nl, displayBoard(Board), nl, nl,
 	write('Player '), write(Player), write(' choose a column number : '), nl,
@@ -12,13 +14,31 @@ play(Board, Player) :-
 	otherPlayer(Player, Opponent),
 	play(NewBoard, Opponent).
 
-playMove([L|G], 1, Player, _, _):- length(L, N), N >= 6, nl, write('Illegal move !'), nl, play([L|G], Player).
-playMove([L|G], 1, Player, F, I):- length(L, N), N < 6, pushBack(Player, L, M), F=[M|G].
-playMove([L|G], N, Player, _, _):- N > 7, nl, write('Illegal move !'), nl, play([L|G], Player).
-playMove([T|X], N, Player, [T|G], I):- N > 0, N1 is N-1, playMove(X, N1, Player, G, I).
+playMove([H|T], 1, Player, _, _):- length(H, N), N >= 6, nl, write('Illegal move !'), nl, play([H|T], Player).
+playMove([H|T], 1, Player, F, _):- length(H, N), N < 6, pushBack(Player, H, M), F=[M|T].
+
+playMove([H|T], N, Player, _, _):- N > 7, nl, write('Illegal move !'), nl, play([H|T], Player).
+playMove([H|T1], N, Player, [H|T2], I):- N > 0, N1 is N-1, playMove(T1, N1, Player, T2, I).
 
 otherPlayer(x, o).
 otherPlayer(o, x).
+
+/*--- WINNING/DRAW CONDITIONS ---*/
+
+verticalWin([H|_], Player) :- sublist([Player, Player, Player, Player], H), !.
+verticalWin([_|T], Player) :- verticalWin(T, Player).
+
+horizontalWin(Board, Player, N) :- maplist(nthElem(N), Board, L), sublist([Player,Player,Player,Player], L),!.
+horizontalWin(Board, Player, N) :- N > 0, N1 is N-1, horizontalWin(Board, Player, N1).
+horizontalWin(Board, Player) :- horizontalWin(Board, Player, 6).
+
+gameOver(Board, Player) :- verticalWin(Board, Player).
+gameOver(Board, Player) :- horizontalWin(Board, Player).
+
+/*--- UTILITIES ---*/
+
+sublist(SL, L):- append(SL,_,L).
+sublist(SL,[_|T]):- sublist(SL, T).
 
 pushBack(X, [], [X]).
 pushBack(X, [Y|L1], [Y|L2]):- pushBack(X, L1, L2).
@@ -31,7 +51,7 @@ nthElem(N, L, X) :- nth1(N, L, X).
 displayElement([]) :- write(' '), !.
 displayElement(E) :- write(E).
 
-displayListe([]):- write('|').
+displayListe([]) :- write('|').
 
 displayListe([E|L]) :-  
 	write('|'), 
@@ -40,22 +60,23 @@ displayListe([E|L]) :-
 
 displayBoard(_,0).
 
-displayBoard(G, N):-	 
+displayBoard(Board, N) :-	 
 	N > 0,
 	N1 is N-1,
-	maplist(nthElem(N), G, L),
+	maplist(nthElem(N), Board, L),
 	displayListe(L),
 	nl,
-	displayBoard(G, N1).
+	displayBoard(Board, N1).
 
-displayBoard(G):- 
-	displayBoard(G, 6),  
+displayBoard(Board) :- 
+	displayBoard(Board, 6),  
 	write(' ¯ ¯ ¯ ¯ ¯ ¯ ¯'),
 	nl,
 	write(' 1 2 3 4 5 6 7').
 
-
-
-
- 
-
+displayWinner(Board, Player) :- 
+	displayBoard(Board), 
+	nl, nl, 
+	write('Player '), 
+	write(Player), 
+	write(' win the game !').
